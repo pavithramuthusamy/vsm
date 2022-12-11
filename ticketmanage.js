e73d387f62a445cb4f5c416c07b6d2f35366f85a
Pavithra Muthusamy <pavithra.muthusamy@siemens.com>
2022-12-12 00:44:12 +0530

    Added scroll up

--- ticketmanage.js
+++ ticketmanage.js
@@ -1,14 +1,3 @@
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-  
-   navigation: {
-    nextEl: '.next',
-    prevEl: '.prev',
-  },
-  
-})
-
 function headerClick() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
